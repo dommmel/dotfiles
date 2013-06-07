7bc0333bfb6c82a44ee40fd9e37ee08c71c37da7
Dommmel <dommmel@gmail.com>
2013-06-07 11:16:44 +0200

    add postres.app path and tweak prompt

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,6 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/opt/local/bin:/usr/local/share/npm/bin"
+
+# using http://postgresapp.com/
+export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
