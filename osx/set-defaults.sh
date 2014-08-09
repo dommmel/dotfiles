c7503718551a529725e92d7b55ef595ea2c15c9b
Dommmel <dommmel@gmail.com>
2014-08-09 22:06:39 +0200

    add vagrant aliasl and enable quicklook text selection

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -9,3 +9,6 @@
 
 # Show the ~/Library folder.
 chflags nohidden ~/Library
+
+# enable text selection in Quick Look 
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE
--- vagrant/aliases.zsh
+++ vagrant/aliases.zsh
@@ -8,7 +8,9 @@ fi
 if [[ -f $(which vagrant) ]]
 then
 	alias vup='vagrant up && vagrant ssh'
+	alias vupp='vagrant up && vagrant ssh -- -t "cd /vagrant; exec \$SHELL"'
 	alias vssh='vagrant ssh'
+	alias vsssh='vagrant ssh -- -t "cd /vagrant; exec \$SHELL"'
 	alias vres='vagrant resume'
 	alias vsus='vagrant suspend'
 	alias vrel='vagrant reload'
