9fa10f261814e67a054f13ed7a0a032a87b0cfa1
Dommmel <dommmel@gmail.com>
2015-10-28 11:00:30 +0100

    unbug comdef

--- zsh/gcloud.zsh
+++
@@ -1,5 +0,0 @@
-# The next line updates PATH for the Google Cloud SDK.
-source "$HOME/SDKs/google-cloud-sdk/path.zsh.inc"
-
-# The next line enables shell command completion for gcloud.
-source "$HOME/SDKs/google-cloud-sdk/completion.zsh.inc"
