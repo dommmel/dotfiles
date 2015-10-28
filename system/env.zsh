a17ad65b558f009b65e9d91270a197c0ce8225b2
Dommmel <dommmel@gmail.com>
2015-10-28 10:02:55 +0100

    minor tweaks

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,3 @@
 export EDITOR_BG='subl'
 export EDITOR='subl --wait'
+export LANG=en_US.UTF-8
--- system/path.zsh
+++ system/path.zsh
@@ -1,6 +1,9 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/opt/local/bin:/usr/local/share/npm/bin"
+export PATH="./bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/opt/local/bin:/usr/local/share/npm/bin"
 
 # using http://postgresapp.com/
 export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# add google app engine
+export PATH="$HOME/SDKs/go_appengine:$PATH"
