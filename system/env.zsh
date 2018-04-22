dbfc88e1efb943edbcf19f7d2bc2349e804ffb74
Dommmel <dommmel@gmail.com>
2018-04-22 23:03:30 +0200

    bulk update

--- system/env.zsh
+++ system/env.zsh
@@ -1,3 +1,4 @@
 export EDITOR_BG='subl'
 export EDITOR='subl --wait'
+export LC_ALL=en_US.UTF-8  
 export LANG=en_US.UTF-8
--- system/path.zsh
+++ system/path.zsh
@@ -19,3 +19,6 @@ export GOPATH=$HOME/go
 export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
+
+# Python 2
+export PATH=$PATH:$HOME/Library/Python/2.7/bin  
